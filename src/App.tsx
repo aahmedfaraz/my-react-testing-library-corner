5fd65a9673ee919d0ab8976b7655bed2af2f2817
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-10-09 14:18:31 +0500

    Added Tests Output Screenshots
    - Added Navbar Tests Output Screenshot
    - Added Form Tests Output Screenshot

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
-
+// Images
+import NavbarTestsOutput from './assets/images/navbar_tests_output.png';
+import FormTestsOutput from './assets/images/form_tests_output.png';
+// React Components
 import Navbar from './components/layout/Navbar';
 import Form from './components/layout/Form';
 
@@ -13,8 +16,13 @@ function App() {
         <br />
         But the main focus🎯 of this project is implemented <strong>Automatic Testing ⚡</strong>. Throughout all this project I was exploring🧭 the library. So, any from my tech guys if wanna see those testing practices of mine. See the Code💻 and README Docs📖 in the Github Repo using GitHub Code Button up in the navbar.</p>
         <p>- Enjoy ☕ -</p>
+        {/* Navbar Tests Output */}
+        <h4>Navbar Tests Output</h4>
+        <img className="output-image" src={NavbarTestsOutput} alt="Navbar Tests Output" />
         <h1>Testing React Elements <small>using '@testing-library/react'</small></h1>
         <Form />
+        <h4>Form Tests Output</h4>
+        <img className="output-image" src={FormTestsOutput} alt="Form Tests Output" />
       </main>
     </div>
   );
